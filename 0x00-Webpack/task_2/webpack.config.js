282fba9aa9abaae82949b7d6e50a1e952670d6a6
Paa Kojo Annan <pkeannan77@gmail.com>
2023-05-18 11:56:11 +0000

    changes

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,16 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  rules: [
-    {
-      test: /\.css$/,
-      use: ["style-loader", "css-loader"]
-    },
-    {
-      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-      type: 'asset/resource'
-    }
-  ],
   mode: 'production',
   entry: {
     main: path.resolve(__dirname, './js/dashboard_main.js'),
@@ -18,5 +8,17 @@ module.exports = {
     output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+	use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+	type: 'asset/resource'
+      }
+    ]
   }
 };
