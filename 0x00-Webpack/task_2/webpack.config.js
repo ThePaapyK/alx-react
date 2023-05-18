874eab27cbdbae81984ae824e5c3041c1f0cadc4
Paa Kojo Annan <pkeannan77@gmail.com>
2023-05-18 12:19:49 +0000

    additions

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -18,7 +18,34 @@ module.exports = {
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
 	type: 'asset/resource'
-      }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+	use: [
+          'file-loader',
+	   {
+	     loader: 'image-webpack-loader',
+	     options: {
+	       mozjpeg: {
+		 progressive: true,
+	       },
+	       optipng: {
+	         enabled: false,
+	       },
+	       pngquant: {
+		 quality: [0.65, 0.9],
+		 speed: 4,
+	       },
+	       gifsicle: {
+		 interlaced: false,
+	       },
+	       webp: {
+		 quality: 75,
+	       },
+	     },
+	   },
+	],
+      },
     ]
   }
 };
